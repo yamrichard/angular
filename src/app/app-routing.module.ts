9a351826ee70e28bbec3106a15c4cd824e2017aa
yamrichard <yamrichard07@gmail.com>
2020-06-09 14:11:42 +0000

    les route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClickageComponent } from './dialog/clickage/clickage.component';
+import { AjouterComponent } from './dialog/ajouter/ajouter.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ClickageComponent},
+  {path: 'ajouter', component: AjouterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
